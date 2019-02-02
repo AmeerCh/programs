c8e948331aaeb96f131668e4f5483180393a970d
Ameer <ameerbashach@gmail.com>
2019-02-02 04:29:04 +0000

    add product problem to github

---
+++ c-programs/product.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int Number1, Number2, Number3, Product;
+  printf("To find the product of three numbers, enter the first number: ");
+  scanf("%d", &Number1);
+  printf("Enter the second number: ");
+  scanf("%d", &Number2);
+  printf("Enter the third number: ");
+  scanf("%d", &Number3);
+  Product = Number1 * Number2 * Number3;
+  printf("The product of %d, %d and %d is %d.\n", Number1, Number2, Number3, Product);
+  return 0;
+}
